df4e00677731bca39f523b0165d464c02958e09f
dilushaeka <dilushaeka99@gmail.com>
2024-01-28 22:16:55 +0530

    Server Added Correctly

---
+++ src/lk/dil/server/ServerInitializer.java
@@ -0,0 +1,43 @@
+package lk.dil.server;
+
+import lk.ijse.gdse.server.controller.LocalSocketManager;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerInitializer {
+
+    public static List<LocalSocketManager> localSocketManagerList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket;
+        Socket socket;
+        try {
+
+            serverSocket = new ServerSocket(3002);
+
+            while (!serverSocket.isClosed()) {
+
+                System.out.println("Waiting");
+                socket = serverSocket.accept();
+                System.out.println("accept");
+
+                LocalSocketManager localSocketManager = new LocalSocketManager(socket, localSocketManagerList);
+                localSocketManagerList.add(localSocketManager);
+
+                Thread thread =new Thread(localSocketManager);
+                System.out.println("Thread start");
+                thread.start();
+            }
+            //socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/lk/dil/server/controller/LocalSocketManager.java
@@ -0,0 +1,117 @@
+package lk.dil.server.controller;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class LocalSocketManager implements Runnable {
+
+    public Socket socket;
+    public List<LocalSocketManager> localSocketManagerList;
+    public DataInputStream inputStream;
+    public DataOutputStream outputStream;
+    public String type;
+
+    public LocalSocketManager(Socket socket, List<LocalSocketManager> localSocketManagerList) {
+
+        this.socket = socket;
+        this.localSocketManagerList = localSocketManagerList;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+
+
+            while (socket.isConnected()) {
+
+                this.type = inputStream.readUTF();
+                if (this.type.equalsIgnoreCase("text")) {
+
+                    sendText();
+
+                }else if(this.type.equalsIgnoreCase("picture")){
+
+                    sendFile();
+
+                }else if(this.type.equalsIgnoreCase("Close")) {
+
+                    System.out.println(localSocketManagerList.size());
+                    localSocketManagerList.remove(this);
+                    System.out.println(localSocketManagerList.size());
+                    this.type = "Text";
+                    sendText();
+                    Thread.currentThread().isInterrupted();
+
+                }
+            }
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sendFile() {
+
+        try {
+
+            String userName = inputStream.readUTF();
+
+            byte[] sizeAr = new byte[4];
+            inputStream.read(sizeAr);
+            int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
+
+            byte[] imageAr = new byte[size];
+            inputStream.read(imageAr);
+
+            for (LocalSocketManager localSocketManager : localSocketManagerList) {
+
+                if(!localSocketManager.equals(this)){
+
+                    localSocketManager.outputStream.writeUTF(type);
+                    localSocketManager.outputStream.writeUTF(userName);
+                    localSocketManager.outputStream.write(sizeAr);
+                    localSocketManager.outputStream.write(imageAr);
+                    localSocketManager.outputStream.flush();
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void sendText() {
+
+        try {
+
+            String userName = inputStream.readUTF();
+            String message = inputStream.readUTF();
+            System.out.println(userName);
+            System.out.println(message);
+
+            for (LocalSocketManager localSocketManager : localSocketManagerList) {
+
+                if (!localSocketManager.equals(this)) {
+
+                    localSocketManager.outputStream.writeUTF(type);
+                    localSocketManager.outputStream.writeUTF(userName);
+                    localSocketManager.outputStream.writeUTF(message);
+                    localSocketManager.outputStream.flush();
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
